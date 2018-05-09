961d81cb10d9347a780a754a40d0957f75bdd390
bojan.milojkovic <bojan.milojkovic@tlscontact.com>
2018-05-09 11:05:07 +0200

    This is still a test

--- green-health/src/main/java/com/green/health/GreenHealthApplication.java
+++ green-health/src/main/java/com/green/health/GreenHealthApplication.java
@@ -2,14 +2,10 @@ package com.green.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.green.health.user.*"})
-@EnableJpaRepositories("com.green.health.user.*")
-@EntityScan( basePackages = {"com.green.health.user.*"} )
 public class GreenHealthApplication {
 
 	public static void main(String[] args) {
--- green-health/src/main/java/com/green/health/config/PersistenceContext.java
+++ green-health/src/main/java/com/green/health/config/PersistenceContext.java
@@ -3,10 +3,13 @@ package com.green.health.config;
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -15,7 +18,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-//@EnableJpaRepositories("com.green.health.user.*")
+@EnableJpaRepositories("com.green.health.user.*")
+@EntityScan( basePackages = {"com.green.health.user.*"} )
 @PropertySource({ "classpath:application.properties"})
 public class PersistenceContext {
 	
