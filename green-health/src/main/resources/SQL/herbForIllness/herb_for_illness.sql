17b501cb8464228780086eab5d937c106035160e
Bojan Milojko <lord_lazaruss@yahoo.com>
2018-10-16 00:12:26 +0200

    BOJAN
    Fixing db table

--- green-health/src/main/resources/SQL/herbForIllness/herb_for_illness.sql
+++ green-health/src/main/resources/SQL/herbForIllness/herb_for_illness.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `herb_for_illness` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `herb_id` int(11) NOT NULL,
   `illness_id` int(11) NOT NULL,
-  `ratings` int(11) NOT NULL DEFAULT '3',
-  PRIMARY KEY (`herb_id`,`illness_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idnew_table_UNIQUE` (`id`),
   KEY `fk_illness_id_idx` (`illness_id`),
+  KEY `fk_herb_id_idx` (`herb_id`),
   CONSTRAINT `fk_herb_id` FOREIGN KEY (`herb_id`) REFERENCES `herb` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_illness_id` FOREIGN KEY (`illness_id`) REFERENCES `illness` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
