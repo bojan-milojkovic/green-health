94eaed6613c0bf7fe127f5699256340b85c313bc
bojan.milojkovic <bojan.milojkovic@tlscontact.com>
2019-04-25 15:30:23 +0200

    BOJAN
    Remove unused objectMapper

--- green-health/src/main/java/com/green/health/config/SpringConfig.java
+++ green-health/src/main/java/com/green/health/config/SpringConfig.java
@@ -3,11 +3,11 @@ package com.green.health.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import com.fasterxml.jackson.databind.ObjectMapper;
+/*import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;*/
 
 @Configuration
 @ComponentScan(basePackages={"com.green.health.*"})
@@ -19,7 +19,7 @@ public class SpringConfig {
 
     public static final long MAX_UPLOAD_PER_FILE_SIZE = 5242880;
 
-	@Bean
+/*	@Bean
     @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         // this makes sure that datetime in string format in input 
@@ -28,7 +28,7 @@ public class SpringConfig {
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
-    }
+    }*/
 	
 	@Bean(name = "multipartResolver")
 	public CommonsMultipartResolver multipartResolver() {
